5a25d8679aa1eb074b807d389e59226b741b7144
samsja <sami.jaghouar@hotmail.fr>
2023-04-03 13:13:22 +0200

    feat: remove exec.py
    Signed-off-by: samsja <sami.jaghouar@hotmail.fr>

--- open_flamingo_serve/executor.py
+++
@@ -1,8 +0,0 @@
-from jina import DocumentArray, Executor, requests
-
-
-class MyExecutor22(Executor):
-    """OpenFlamingo serving. MLLM, text and image as input and generate text out of it"""
-    @requests
-    def foo(self, docs: DocumentArray, **kwargs):
-        pass
